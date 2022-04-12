6bd7dd56248f8637898ac2beffcb010388d74966
João Maranhão <joaofelipemaranhao@gmail.com>
2022-04-12 17:18:12 -0300

    refactor: adjust tomorrows date

--- src/services/api/tomorrow-weather.ts
+++ src/services/api/tomorrow-weather.ts
@@ -8,7 +8,7 @@ const baseUrl = 'http://api.weatherapi.com/v1'
 export const getTomorrowWeather = async (city: string) => {
   const result = await axios.get(`${baseUrl}/forecast.json?key=${apiKey}&q=${city}&days=2&aqui=no`)
   return {
-    date: formatDate(new Date(result.data.location.localtime)),
+    date: formatDate(new Date(result.data.forecast.forecastday[1].date + 'T03:00')),
     city: result.data.location.name,
     country: result.data.location.country,
     condition: result.data.forecast.forecastday[1].day.condition.text,
